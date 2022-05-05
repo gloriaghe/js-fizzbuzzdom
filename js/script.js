275447901afde621ef6bb64254a022b9594eccd9
Gloria <gloghe@yahoo.it>
2022-05-05 12:55:37 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Stampare n da 1 a 100
+// let risultato
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0){
+        console.log("FizzBuzz")      
+    } else if (i % 5 === 0){
+        console.log("Buzz")    
+    } else if (i % 3 === 0){
+        console.log("Fizz")  
+    } 
+    else
+    console.log(i)
+}
+
+// stampare Fizz multipli 3 
+
+// stampare Buzz multipli 5
+
+// stampare FizzBuzz multipli 3 e 5
