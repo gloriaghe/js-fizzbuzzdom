b9c8f1c86bfc5400b1b1adf98a274f78f1c1c8c3
Gloria <gloghe@yahoo.it>
2022-05-05 15:22:38 +0200

    ;

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-const numFB = document.querySelector("div.quadretti")
+const numFB = document.querySelector("div.quadretti");
 
 
 // Stampare n da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // stampare FizzBuzz multipli 3 e 5
     if (i % 5 === 0 && i % 3 === 0){
-        console.log("FizzBuzz")   
+        console.log("FizzBuzz");   
         const element = `<span class="box fizzbuzz">FizzBuzz</span>`; 
         numFB.innerHTML += element;
     } // stampare Fizz multipli 3 
     else if (i % 3 === 0){
-        console.log("Fizz") 
+        console.log("Fizz");
         const element = `<span class="box fizz">Fizz</span>`; 
         numFB.innerHTML += element;
     } // stampare Buzz multipli 5
     else if (i % 5 === 0){
-        console.log("Buzz") 
+        console.log("Buzz");
         const element = `<span class="box buzz">Buzz</span>`; 
         numFB.innerHTML += element;
     } else if  (i % 5 != 0 && i % 3 != 0) {
-        console.log(i)
+        console.log(i);
         const element = `<span class="box">${i}</span>`; 
          numFB.innerHTML += element; }
     }
