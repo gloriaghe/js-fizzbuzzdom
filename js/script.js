884e07e4f1ef927ae1db9cc8ca204c37345ff201
Gloria <gloghe@yahoo.it>
2022-05-05 14:03:13 +0200

    numeri sul dom

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
+const numFB = document.querySelector("div.quadretti")
+
+
 // Stampare n da 1 a 100
-// let risultato
 for (let i = 1; i <= 100; i++) {
+    // stampare FizzBuzz multipli 3 e 5
     if (i % 5 === 0 && i % 3 === 0){
-        console.log("FizzBuzz")      
-    } else if (i % 5 === 0){
-        console.log("Buzz")    
-    } else if (i % 3 === 0){
-        console.log("Fizz")  
-    } 
-    else
-    console.log(i)
-}
+        console.log("FizzBuzz")   
+        const element = `<span class="box box--${i}">FizzBuzz</span>`; 
+        numFB.innerHTML += element;
+    } // stampare Fizz multipli 3 
+    else if (i % 3 === 0){
+        console.log("Fizz") 
+        const element = `<span class="box box--${i}">Fizz</span>`; 
+        numFB.innerHTML += element;
+    } // stampare Buzz multipli 5
+    else if (i % 5 === 0){
+        console.log("Buzz") 
+        const element = `<span class="box box--${i}">Buzz</span>`; 
+        numFB.innerHTML += element;
+    } else if  (i % 5 != 0 && i % 3 != 0) {
+        console.log(i)
+        const element = `<span class="box box--${i}">${i}</span>`; 
+         numFB.innerHTML += element; }
+    }
 
-// stampare Fizz multipli 3 
 
-// stampare Buzz multipli 5
 
-// stampare FizzBuzz multipli 3 e 5
